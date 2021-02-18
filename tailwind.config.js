0685f2de0f8a85c910ec3d874c9066a748b12eaa
Lokman Musliu <lokman.musliu@gmail.com>
2021-02-18 17:22:22 +0100

    Updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    purge: [
-        './src/**/*.njk',
-        './src/**/*.svg',
-        './src/**/*.html'
-    ],
-	theme: {
-        extend: {}
-    }
-}
+  purge: ["./src/**/*.njk", "./src/**/*.svg", "./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
